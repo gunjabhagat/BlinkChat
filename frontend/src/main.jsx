79dd4daa4fd4421d7ac39ed8e572a0eb9300e77b
gunjabhagat <hk4979169@gmail.com>
2025-08-13 00:02:18 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 
-export const serverUrl="https://blinkchat-backend-7hbw.onrender.com"
+export const serverUrl="https://blinkchat-backend-s05u.onrender.com"
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <Provider store={store}>
